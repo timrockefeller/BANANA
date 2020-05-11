7ed5cacb35577b404cfff6bbdb97289b084aeb08
Kitekii <tim720719@163.com>
2020-05-12 00:26:28 +0800

    add:editor 小试框架
    测试一下textarea悬浮的操作

---
+++ src/renderer/components/Editor/Editor.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="editor-frame">
+    <div class="text-browser" @click="mouseEvent">
+      Sample Text <span style="color:#F00" >Like We Have</span> Sun Shine!
+    </div>
+    <textarea 
+      id="editor-inserter"
+      ref="inserter"
+      v-if="onfocus"
+      :style="inserterStyle"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'editor-page',
+  data () {
+    return {
+      inserterPosX: 0,
+      inserterPosY: 0,
+      onfocus: true
+    }
+  },
+  computed: {
+    inserterStyle () {
+      return 'top:' + this.inserterPosY + 'px;left:' + this.inserterPosX + 'px'
+    }
+  },
+  methods: {
+    mouseEvent: function (e) {
+      this.inserterPosX = e.offsetX
+      this.inserterPosY = e.offsetY
+      this.$refs.inserter.focus()
+    }
+  }
+}
+</script>
+
+<style>
+.text-browser{
+  position:absolute;
+  width:100%;
+  height:70em;
+  left:0;
+  background: rgb(255, 255, 215);
+}
+#editor-inserter{
+  position:relative;
+  outline:none; 
+  resize:none; 
+  background:transparent; 
+  border-color:transparent;
+  overflow-y:hidden;
+  overflow-x:hidden;
+  caret-color: #FA0560;
+}
+</style>
--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -1,13 +1,20 @@
 <template>
   <main>
+    <!-- Menu Bar -->
 
+    <editor></editor>
+
+    <!-- Process Bar -->
       
   </main>
 </template>
 
 <script>
-export default {
+import Editor from './Editor/Editor.vue'
 
+export default {
+  name: 'top',
+  components: { Editor }
 }
 </script>
 
