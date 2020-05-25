da38539b4c76038b439eaa3cec19d43046201070
Measured <41724056@xs.ustb.edu.cn>
2020-05-25 18:39:41 +0800

    fix:Parameter problem

--- src/renderer/utils/file.d.ts
+++ src/renderer/utils/file.d.ts
@@ -1,7 +1,8 @@
 declare class FileContent {
-    constructor(filePath: string);
+    constructor(filePath: string, encoding_s: string);
     onCreate(filePath: string): void;
     loadData(): void;
+    changeEncoding(encoding_s: string): void;
     path: string;
     encoding: string;
     data: string;
