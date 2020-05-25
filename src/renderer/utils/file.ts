7325d9cbf88fd3cb4cbb56c5ab882536b3f2a9ce
Measured <41724056@xs.ustb.edu.cn>
2020-05-25 18:34:48 +0800

    add:File read format encoding

--- src/renderer/utils/file.ts
+++ src/renderer/utils/file.ts
@@ -3,6 +3,7 @@
  */
 const fs = require("fs")
 const pt = require("path")
+const iconv = require('iconv-lite');
 /**
  * Suffix Defines
  */
@@ -32,22 +33,33 @@ const LanguageType: any = {
     ]
 }
 const EncodeType: Array<string> = [
-    "utf-8"
+    'utf-8','gbk','gb2312','gb18030','Big5','Big5-HKSCS','Shift JIS'
 ]
 class FileContent {
-    constructor(filePath: string) {
+    constructor(filePath: string,encoding_s: string) {
         this.onCreate(filePath);
         this.loadData()
+        this.changeEncoding(encoding_s)
+        this.encoding = 'utf-8'
     }
     onCreate(filePath: string) {
         this.language = LanguageType.getLang(filePath.substr(filePath.lastIndexOf('.') + 1))
         this.path = pt.resolve(filePath);
         // FIXME 猜测编码？
-        this.encoding = EncodeType[0]
+      //  this.encoding = EncodeType[0]
     }
 
     loadData() {
-        this.data = fs.readFileSync(this.path, { encoding: this.encoding }).toString();
+       this.data = fs.readFileSync(this.path);
+       this.data = iconv.decode(this.data,this.encoding).toString(); // 不乱码
+    }
+
+    changeEncoding(encoding_s: string){
+        for (let Encodetype of EncodeType) {
+            if (Encodetype.localeCompare(encoding_s) === 0) {
+                this.encoding = encoding_s;
+            }
+        }
     }
 
     public path: string = '';
