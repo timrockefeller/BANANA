75bac13daab7142713d080c4bcb3a45fd4d6ade9
Marlon Xu <1362941473@qq.com>
2020-05-25 14:49:18 +0800

    Add top toolbar structure

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,9 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, 
+  Menu, 
+  //Tray, 
+} from 'electron'
 declare namespace global {
     let __static: string
 }
@@ -17,6 +20,99 @@ const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
+function createMenu() {
+  if (process.platform === 'win32') {
+    const menuTemplate = [{
+      label: '文件',
+      submenu: [{
+        label: '新建文件'
+      },
+      {
+        label: '打开文件'
+      },
+      {
+        label: '保存文件'
+      },
+      {
+        label: '退出'
+      }
+      ]
+    },
+    {
+      label: '编辑',
+      submenu: [{
+        label: '撤销'
+      },
+      {
+        label: '重做'
+      },
+      {
+        label: '剪切'
+      },
+      {
+        label: '复制'
+      },
+      {
+        label: '粘贴'
+      },
+      {
+        label: '查找...'
+      },
+      {
+        label: '替换...'
+      }
+      ]
+    },
+    {
+      label: '视图',
+      submenu: [{
+        label: '显示/隐藏文件导航栏'
+      },
+      {
+        label: '显示/隐藏状态栏'
+      },
+      {
+        label: '显示/隐藏工具栏'
+      }
+      ]
+    },
+    {
+      label: '编译',
+      submenu: [{
+        label: '控制台'
+      },
+      {
+        label: '编译'
+      },
+      {
+        label: '运行'
+      },
+      {
+        label: '编译并运行'
+      }
+      ]
+    },
+    {
+      label: '帮助',
+      submenu: [{
+        label: '关于'
+      },
+      {
+        label: '用户手册'
+      },
+      {
+        label: '网站'
+      }
+      ]
+    }
+    ];
+    let menu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(menu)
+  } else {
+    Menu.setApplicationMenu(null)
+  }
+}
+
 function createWindow () {
   /**
    * Initial window options
@@ -32,15 +128,11 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+  //createMenu();
 }
-/**
- * TODO 添加更多菜单
- * 文件->打开/保存
- * 查查怎么从主进程往vue渲染进程里传事件
- */
-
 
 app.on('ready', createWindow)
+app.on('ready',createMenu)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
