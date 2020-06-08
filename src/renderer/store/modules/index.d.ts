cbf73f0b2b8d6c91e7a2e3768863d340d5715f4d
kitekii <tim720719@163.com>
2020-06-08 17:01:22 +0800

    remove:unused module

--- src/renderer/store/modules/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * The file enables `@/store/index.js` to import all vuex modules
- * in a one-shot manner. There should not be any reason to edit this file.
- */
-declare const modules: {};
-export default modules;
